5e6855345f84d280e19994fe256d1ee961962670
jiangtao <jiangtao.work@gmail.com>
2019-01-12 01:34:08 +0800

    Update: webview views & model

--- okweb/webview/models.py
+++ okweb/webview/models.py
@@ -7,3 +7,6 @@ class Website(models.Model):
     website_name = models.CharField(max_length=64)
     website_url = models.CharField(max_length=255)
     website_create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id}. {self.website_name} ({self.website_url})"
--- okweb/webview/views.py
+++ okweb/webview/views.py
@@ -17,11 +17,28 @@ def index(request):
     result = {
         'msg': 'ok',
         'code': '200',
-        'data': [{
-            'name': x.website_name,
-            'url': x.website_url,
-            'favicon': f"http://favicon.byi.pw/?url={x.website_url}",
-            'create_time': x.website_create_time,
-        } for x in websites]*10,
+        'data': [
+            {
+                'name': '常用推荐',
+                'sites': [{
+                    'name': x.website_name,
+                    'url': x.website_url,
+                    'logo': f"http://favicon.byi.pw/?url={x.website_url}",
+                    'desc': x.website_name*5,
+                    'create_time': x.website_create_time,
+                } for x in websites]*2,
+            },
+            {
+                'name': '实用工具',
+                'sites': [{
+                    'name': x.website_name,
+                    'url': x.website_url,
+                    # 'logo': f"http://favicon.byi.pw/?url={x.website_url}",
+                    'logo': f"https://www.baidu.com/favicon.ico",
+                    'desc': x.website_name*5,
+                    'create_time': x.website_create_time,
+                } for x in websites],
+            },
+        ]
     }
     return JsonResponse(data=result)
