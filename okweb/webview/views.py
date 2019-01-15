2e81ab5e38bb627e89736a09a787ab2ee3043d06
jiangtao <jiangtao.work@gmail.com>
2019-01-15 16:48:43 +0800

    Update: filter category for none websites

--- okweb/webview/views.py
+++ okweb/webview/views.py
@@ -16,45 +16,24 @@ def add_website(request):
 def index(request):
     categorys = Category.objects.filter()
     websites = Website.objects.filter()
+    group_websites = [
+        {
+            'name': category.name,
+            'sites': [{
+                'name': x.website_name,
+                'url': x.website_url,
+                # 'logo': f"http://favicon.byi.pw/?url={x.website_url}",
+                # 'logo': f"https://www.baidu.com/favicon.ico",
+                'logo': x.website_logo,
+                'desc': x.website_desc,
+                'create_time': x.website_create_time,
+            } for x in websites if x and x.website_category == category.id],
+        } for category in categorys if category
+    ]
+    filter_group_websites = [x for x in group_websites if x.get('sites')]
     result = {
         'msg': 'ok',
         'code': '200',
-        'data': [
-            # {
-            #     'name': '常用推荐',
-            #     'sites': [{
-            #         'name': x.website_name,
-            #         'url': x.website_url,
-            #         # 'logo': f"http://favicon.byi.pw/?url={x.website_url}",
-            #         'logo': x.website_logo,
-            #         'desc': x.website_desc,
-            #         'create_time': x.website_create_time,
-            #     } for x in websites]*2,
-            # },
-            # {
-            #     'name': '实用工具',
-            #     'sites': [{
-            #         'name': x.website_name,
-            #         'url': x.website_url,
-            #         # 'logo': f"http://favicon.byi.pw/?url={x.website_url}",
-            #         # 'logo': f"https://www.baidu.com/favicon.ico",
-            #         'logo': x.website_logo,
-            #         'desc': x.website_desc,
-            #         'create_time': x.website_create_time,
-            #     } for x in websites],
-            # },
-            {
-                'name': category.name,
-                'sites': [{
-                    'name': x.website_name,
-                    'url': x.website_url,
-                    # 'logo': f"http://favicon.byi.pw/?url={x.website_url}",
-                    # 'logo': f"https://www.baidu.com/favicon.ico",
-                    'logo': x.website_logo,
-                    'desc': x.website_desc,
-                    'create_time': x.website_create_time,
-                } for x in websites if x and x.website_category == category.id],
-            } for category in categorys if category
-        ]
+        'data': filter_group_websites,
     }
     return JsonResponse(data=result)
